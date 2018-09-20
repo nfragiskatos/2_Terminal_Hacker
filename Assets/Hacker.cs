aace26b88ed2feecb2afad43784b9bdbecc518fe
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-19 18:44:37 -0700

    Initial Commit

---
+++ Assets/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        showMainMenu();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void showMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Test");
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+}
