e416d9f78311b64a45881727bfd374733ee74f5e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2018-09-21 18:06:40 -0700

    Added a 3rd level to play

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -3,8 +3,10 @@
 public class Hacker : MonoBehaviour {
 
     // Game Config Data
-    string[] lvl1Pwds = {"books", "aisle", "self", "password", "font", "borrow" };
+    string[] lvl1Pwds = {"books", "aisle", "shelf", "password", "font", "borrow" };
     string[] lvl2Pwds = {"prisoner", "handcuffs", "holster", "uniform", "arrest" };
+    string[] lvl3Pwds = { "cryptology", "hacking", "cipher", "encryption", "authentication" };
+    const string menuHint = "You may type menu at any time.";
 
     // Game State
     int level;
@@ -17,16 +19,6 @@ public class Hacker : MonoBehaviour {
         level = 0;
         print(lvl1Pwds);
         ShowMainMenu();
-    }
-
-    private void ShowMainMenu()
-    {
-        curScreen = Screen.MainMenu;
-        Terminal.ClearScreen();
-        Terminal.WriteLine("What would you like to hack into?");
-        Terminal.WriteLine("Press 1 for the local library");
-        Terminal.WriteLine("Press 2 for the police station");
-        Terminal.WriteLine("Enter your selection: ");
     }
 
     void OnUserInput(string input)
@@ -46,6 +38,60 @@ public class Hacker : MonoBehaviour {
 
     }
 
+    private void ShowMainMenu()
+    {
+        curScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for the NSA");
+        Terminal.WriteLine("Enter your selection: ");
+    }
+
+    private void RunMainMenu(string input)
+    {
+        bool isValidLvlNum = (input == "1" || input == "2" || input == "3");
+        if (isValidLvlNum)
+        {
+            level = int.Parse(input);
+            AskForPassword();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level");
+            Terminal.WriteLine(menuHint);
+        }
+    }
+
+    private void AskForPassword()
+    {
+        curScreen = Screen.Password;
+        Terminal.ClearScreen();
+        SetRandomPassword();
+        Terminal.WriteLine("Enter the password. hint " + password.Anagram());
+        Terminal.WriteLine(menuHint);
+    }
+
+    private void SetRandomPassword()
+    {
+        switch (level)
+        {
+            case 1:
+                password = lvl1Pwds[Random.Range(0, lvl1Pwds.Length)];
+                break;
+            case 2:
+                password = lvl2Pwds[Random.Range(0, lvl2Pwds.Length)];
+                break;
+            case 3:
+                password = lvl3Pwds[Random.Range(0, lvl3Pwds.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid Level #");
+                break;
+        }
+    }
+
     private void CheckPassword(string input)
     {
         if (input == password)
@@ -54,7 +100,7 @@ public class Hacker : MonoBehaviour {
         }
         else
         {
-            Terminal.WriteLine("Wrong password. Try again");
+            AskForPassword();
         }
     }
 
@@ -63,6 +109,7 @@ public class Hacker : MonoBehaviour {
         curScreen = Screen.Win;
         Terminal.ClearScreen();
         ShowLevelReward();
+        Terminal.WriteLine(menuHint);
     }
 
     private void ShowLevelReward()
@@ -89,42 +136,21 @@ public class Hacker : MonoBehaviour {
 "
                 );
                 break;
+            case 3:
+                Terminal.WriteLine("You hacked into the NSA!");
+                Terminal.WriteLine(@"
+ _   _  _____  ___  
+| \ | |/  ___|/ _ \ 
+|  \| |\ `--./ /_\ \
+| . ` | `--. \  _  |
+| |\  |/\__/ / | | |
+\_| \_/\____/\_| |_/
+"
+                );
+                break;
             default:
                 Debug.LogError("Invalid level reached");
                 break;
         }
-    }
-
-    private void RunMainMenu(string input)
-    {
-        bool isValidLvlNum = (input == "1" || input == "2");
-        if (isValidLvlNum)
-        {
-            level = int.Parse(input);
-            StartGame();
-        }
-        else
-        {
-            Terminal.WriteLine("Please choose a valid level");
-        }
-    }
-
-    private void StartGame()
-    {
-        curScreen = Screen.Password;
-        Terminal.ClearScreen();
-        switch (level)
-        {
-            case 1:
-                password = lvl1Pwds[Random.Range(0, lvl1Pwds.Length)];
-                break;
-            case 2:
-                password = lvl2Pwds[Random.Range(0, lvl2Pwds.Length)];
-                break;
-            default:
-                Debug.LogError("Invalid Level #");
-                break;
-        }
-        Terminal.WriteLine("Please enter the password: ");
     }
 }
